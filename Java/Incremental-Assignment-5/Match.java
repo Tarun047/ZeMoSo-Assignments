b14321a45a2770ea9b4e6f303a0a333677fea74e
Tarun047 <gtarun047@gmail.com>
2019-06-24 12:39:59 +0530

    Incremental Assignment 5 complete

---
+++ Java/Incremental-Assignment-5/Match.java
@@ -0,0 +1,56 @@
+import java.util.regex.Pattern;
+/*
+
+String and Type Information
+
+Using the documentation for java.util.regex.Pattern as a resource,
+write and test a regular expression that checks a sentence to see that it begins with a capital letter
+and ends with a period.
+*/
+
+class Match
+{
+  static Pattern mPattern = Pattern.compile("^[A-Z].*\\.$");
+  static String tests[] = new String[]{
+    "A.",
+    "ABacZ.",
+    "a .",
+    "A B AEEQad sdasdweqads.",
+    "xacsd . sad. awe2113 ;.",
+    ".",
+    "",
+    "GRDSDASae r.",
+    "Ppl.",
+    "123 sad asdq341 $%",
+    "@12311__adas .",
+    "Final very very large match Test with 0123456789    .asd ."
+  };
+  static boolean[] results = {
+    true,
+    true,
+    false,
+    true,
+    false,
+    false,
+    false,
+    true,
+    true,
+    false,
+    false,
+    true,
+  };
+  public static void main(String args[])
+  {
+    regexTest();
+  }
+
+  public static void regexTest()
+  {
+    int i=0;
+    for(String test:tests)
+    {
+      assert results[i]==mPattern.matcher(test).matches():"Failed for case "+(i+1);
+      i++;
+    }
+  }
+}
