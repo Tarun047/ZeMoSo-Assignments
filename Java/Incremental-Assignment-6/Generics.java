9b408a7ca77cd18ecdbd2e7b9d24fe524bed2ed5
Tarun047 <gtarun047@gmail.com>
2019-06-25 11:48:10 +0530

    modified:   Incremental-Assignment-6/Generics.java

--- Java/Incremental-Assignment-6/Generics.java
+++ Java/Incremental-Assignment-6/Generics.java
@@ -1,3 +1,16 @@
+/*
+Create a generic, singly linked list class called SList, which, to keep things simple, 
+does not implement the List interface.
+
+Each Link object in the list should contain a reference to the next element in the list, 
+but not the previous one (LinkedList, in contrast, is a doubly linked list, which means it maintains links in both directions).
+
+Create your own SListIterator which, again for simplicity, does not implement ListIterator. 
+The only method in SList other than toString( ) should be iterator( ), which produces an SListIterator.
+
+The only way to insert and remove elements from an SList is through SListIterator. 
+Write code to demonstrate SList.
+*/
 class SList<T>
 {
   class Node<T>
@@ -59,6 +72,7 @@ class SList<T>
       return prev.data;
     }
   }
+
   public SListIterator iterator()
   {
     return new SListIterator();
