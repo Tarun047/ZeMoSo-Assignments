b0f4ff84778b04f502fac286b5ebebae65cc8c55
Tarun047 <gtarun047@gmail.com>
2019-09-06 16:26:33 +0530

    Completed Web Sockets Application

---
+++ Spring/Chatty/src/main/java/com/tarun/Chatty/ChattyApplication.java
@@ -0,0 +1,15 @@
+package com.tarun.Chatty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChattyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattyApplication.class, args);
+
+	}
+
+}
