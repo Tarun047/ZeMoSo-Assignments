abd701a83a2ac6aceb65aa4e8d6bc2cbd93d8d3f
Tarun047 <gtarun047@gmail.com>
2019-09-09 16:00:03 +0530

    Auto Commit by Script

--- React-Practice/hooksdemo/src/App.js
+++ React-Practice/hooksdemo/src/App.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import {VictoryChart} from 'victory'
 import './App.css';
 
 const useStyles = makeStyles(theme => 
@@ -28,27 +29,26 @@ function App() {
     {
       const response = await fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=S9C3XDXUHXF2Q4BB');
       const data = await response.json();
-      setStock(data['Time Series (5min)']);
+      setStock(data);
     }
     fetchData();
   },[setStock])
-  console.log(stock)
-  return (
 
+  function transformData()
+  {
+    Object.keys(stock['Time Series (5min)']).map(key=>{
+    { 
+      x:key,
+      y:stock[key]['1. open']
+    }
+    });
+     return "Hello"
+  }
+  
+  return (
     <div className={classes.root}>
       {
-        stock ? Object.keys(stock).map(
-          time=>
-          <div key={time}>
-            {time}:
-            {
-              Object.keys(stock[time]).map(interval=>
-                <div key={interval}>
-                  {interval}:{stock[time][interval]}
-                </div>
-            )}
-          </div>
-        ):null
+        stock ? transformData():null
       }
     </div>
   );
