0cd2da868c9408316cdb184c5dc7da122936704c
Tarun047 <gtarun047@gmail.com>
2019-09-09 11:50:02 +0530

    Auto Commit by Script

--- React-Practice/hooksdemo/src/App.js
+++ React-Practice/hooksdemo/src/App.js
@@ -3,43 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [count,setCount] = useState(0);
-  const [title,setTitle] = useState(0);
-  function increment()
-  {
-    setCount(count+1);
-  }
-
-  function handleChange(event)
-  {
-    setTitle(event.target.value);
-  }
- 
-  useEffect(()=>{
-    document.title=title;
-  },[title])
-
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <button onClick={increment}>Click Me</button>
-          <br/>Current Count is:{count}<br/>
-          <input type="text" value={title} onChange={handleChange} />
-        </div>
-      </header>
+      
     </div>
   );
 }
