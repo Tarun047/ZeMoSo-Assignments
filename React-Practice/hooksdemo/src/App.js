8f94a51f478b2285b620ae80497d3b2440330434
Tarun047 <gtarun047@gmail.com>
2019-09-09 17:10:02 +0530

    Auto Commit by Script

--- React-Practice/hooksdemo/src/App.js
+++ React-Practice/hooksdemo/src/App.js
@@ -9,7 +9,8 @@ const useStyles = makeStyles(theme =>
     root: {
       display: 'flex',
       flexWrap: 'wrap',
-      justifyContent: 'space-around',
+      flexDirection: 'column',
+      alignItems:'center',
       overflow: 'hidden',
       backgroundColor: theme.palette.background.paper,
     },
@@ -35,24 +36,25 @@ function App() {
      
         const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${debouncedSearchTerm}&interval=5min&apikey=S9C3XDXUHXF2Q4BB`);
         const data = await response.json();
-        setStock(data);
+        if('Time Series (5min)' in data)
+          setStock(data);
     }
     if(debouncedSearchTerm)
     {
       setIsSearching(true);
       fetchData().then(()=>setIsSearching(false))
     }
+
+    return function cleanup()
+    {
+      setStock(null);
+    }
   },[debouncedSearchTerm])
 
 
   function transformData(column)
   {
-    
-    if(stock)
-    {
       return Object.keys(stock['Time Series (5min)']).map(key=>{return {x: new Date(key),y:parseFloat(stock['Time Series (5min)'][key][column]) };});
-    }
-    return null;
   }
 
   function handleIntrestChange(event)
@@ -66,13 +68,13 @@ function App() {
         <input value={intrest} onChange={handleIntrestChange} />
       </div>
       {
-      !isSearching ? <VictoryChart data={transformData('1. open')} height={250}>
+      stock && !isSearching ? <VictoryChart data={transformData('1. open')} height={250}>
       <VictoryLine 
         interpolation="linear"
         data={transformData('1. open')}  
         style={{data: {stroke: "#c43a31", strokeWidth: 1}}}
       />
-    </VictoryChart>:<div> "Searching ..."</div>
+    </VictoryChart> : <div> Enter a valid term to Search</div>
       }
     </div>
   );
