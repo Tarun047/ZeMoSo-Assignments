83ea6106ef11b50386d60c2bb3de96e676e7977d
Tarun047 <gtarun047@gmail.com>
2019-09-09 12:20:02 +0530

    Auto Commit by Script

--- React-Practice/hooksdemo/src/App.js
+++ React-Practice/hooksdemo/src/App.js
@@ -2,10 +2,21 @@ import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  let [stock,setStock] = useState({});
 
+  useEffect(()=>
+  {
+    async function fetchData()
+    {
+      const response = await fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=S9C3XDXUHXF2Q4BB');
+      const data = await response.json();
+      setStock(data);
+    }
+    fetchData();
+  })
   return (
     <div className="App">
-      Hello World
+      {stock}
     </div>
   );
 }
