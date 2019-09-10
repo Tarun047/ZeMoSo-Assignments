1e3a04fca5650221fdcc406037014b40be918867
Tarun047 <gtarun047@gmail.com>
2019-09-10 11:40:09 +0530

    Auto Commit by Script

---
+++ Spring/TalkBuddy/frontend/internbuddy/build/precache-manifest.fd5d114c6cdf2490d30af9af90137e00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac10d7e3481bbee203a040f2a6b8a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f5d89180d12cd3e4ae",
+    "url": "/static/css/2.16218f3b.chunk.css"
+  },
+  {
+    "revision": "bcf949556f88e5822586",
+    "url": "/static/css/main.9c462dc1.chunk.css"
+  },
+  {
+    "revision": "94f5d89180d12cd3e4ae",
+    "url": "/static/js/2.8a9e36f5.chunk.js"
+  },
+  {
+    "revision": "bcf949556f88e5822586",
+    "url": "/static/js/main.f18ade6d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
