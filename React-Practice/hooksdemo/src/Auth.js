11fcfc8784b25be65468f7b777b0dc135a12107f
Tarun047 <gtarun047@gmail.com>
2019-09-10 15:30:02 +0530

    Auto Commit by Script

---
+++ React-Practice/hooksdemo/src/Auth.js
@@ -0,0 +1,34 @@
+import {React,useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => 
+    ({
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+          }
+    }));
+
+export default function Auth()
+{
+    [userName,setUserName] = useState()
+    [password,setPassword] = useState();
+
+
+    return(
+        <Container maxWith="sm">
+            <form>
+                <TextField 
+                    id="outlined-required"
+                    label="Stock Code"
+                    className={classes.textField}
+                    margin="normal"
+                    variant="outlined"
+                    value={intrest}
+                    onChange={handleIntrestChange} >
+
+                 </TextField>
+            </form>
+        </Container>
+    )
+}
