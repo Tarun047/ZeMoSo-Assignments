780032c130b91008d97c73a2883bc13ca34578a2
Tarun047 <gtarun047@gmail.com>
2019-09-11 12:00:03 +0530

    Auto Commit by Script

--- React-Practice/hooksdemo/src/Main.js
+++ React-Practice/hooksdemo/src/Main.js
@@ -1,11 +1,16 @@
 import auth from './firebase/manager'
 import Login from'./Login'
 import App from './App'
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import theme from './theme'
 import MuiThemeProvider from '@material-ui/styles/ThemeProvider';
 export default function Main()
 {
+    const [user,setUser] = useState(auth.user)
+    useEffect(() => {
+        auth.onAuthStateChanged((user)=>{setUser(user)})
+    }, [user])
+
     if(!auth.user)
         return (
             <MuiThemeProvider theme={theme}>
