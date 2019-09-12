edd1fc8589fd134fa8a862c50ea71820a2037f64
Tarun047 <gtarun047@gmail.com>
2019-09-12 12:20:02 +0530

    Auto Commit by Script

--- React-Practice/resumemaker/src/App.js
+++ React-Practice/resumemaker/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {makeStyles} from '@material-ui/core/styles'
-import {Container, Toolbar,AppBar, IconButton,Typography,Button} from '@material-ui/core'
+import {Container, Toolbar,AppBar, IconButton,Typography,Button, Box} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import BasicForm from './FormMaker/BasicFormMaker';
 const useStyles = makeStyles(
   theme=>(
     {
+      appBarRoot:
+      {
+        flexGrow: 1,
+      },
       root:
       {
         height:'100%',
@@ -18,19 +22,32 @@ const useStyles = makeStyles(
         alignItems:'center',
       },
       menuButton: {
-        marginRight: theme.spacing(2),
-      },
-      title: {
-        flexGrow: 1,
-      },
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
     })
 )
 function App() {
   const classes = useStyles();
 
   return (
-        <Container maxWidth="sm" className={classes.root}>
-          <BasicForm fields={{name:"FirstName"}} />
+        <Container maxWidth="sm">
+          <AppBar className={classes.appBarRoot}>
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Temp
+              </Typography>
+              <Button color="inherit">Logout</Button>
+            </Toolbar>
+          </AppBar>
+          <Box className={classes.root}>
+            <BasicForm fields={{name:"FirstName"}} />
+          </Box>
         </Container>
   );
 }
